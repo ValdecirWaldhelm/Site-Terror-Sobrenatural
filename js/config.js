f30b32c422dfae1dd5821c18fe599e7bd125a656
Valdecir Waldhelm Morgado <valdwald81@gmail.com>
2021-02-09 23:04:17 -0300

    melhorando menu mobile

--- js/config.js
+++ js/config.js
@@ -1,28 +1,17 @@
 
-     var iconMenu = document.querySelectorAll('.icon-menu');
 
-     iconMenu[0].addEventListener('click',()=>{
-             //Efeito de opacidade.
-             /*
-             let menu = document.getElementById('menu');
-             if(menu.classList.contains('hide')){
-                 menu.classList.add('show');
-                 menu.classList.remove('hide');
-             }else{
-                 menu.classList.add('hide');
-                 menu.classList.remove('show');
-             }
-             */
-     
-             //Efeito toggle
-             let menu = document.getElementById('menu');
-             if(menu.classList.contains('hide')){
-                 menu.classList.add('show');
-                 menu.classList.remove('hide');
-             }else{
-                 menu.classList.add('hide');
-                 menu.classList.remove('show');
-             }
-     
-     
-     });
+    $(function(){
+
+        // $('.icon-menu').click(function(){
+        //         $(this).find('a li').slideToggle();
+        //     });
+        $('.icon-menu').on('click touchstart', function () {
+            $('html').toggleClass('btn-menu-active');
+          });
+
+
+
+
+    });
+
+    
