77690bad23f4c9f5e008b97811aed166033b3176
Valdecir Waldhelm Morgado <valdwald81@gmail.com>
2021-02-11 22:52:42 -0300

    mais responsivo menu mobile

--- js/config.js
+++ js/config.js
@@ -2,10 +2,9 @@
 
     $(function(){
 
-        // $('.icon-menu').click(function(){
-        //         $(this).find('a li').slideToggle();
-        //     });
-        $('.icon-menu').on('click touchstart', function () {
+        // menu slide mobile
+        
+        $('.btn-menu').on('click touchstart', function () {
             $('html').toggleClass('btn-menu-active');
           });
 
